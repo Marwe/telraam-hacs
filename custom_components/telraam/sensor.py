6ccdd303e91f73e0148730f7753a590e5daca76a
Frank Dekervel <kervel@gmail.com>
2024-10-07 17:52:27 +0200

    add total vehicles

--- custom_components/telraam/sensor.py
+++ custom_components/telraam/sensor.py
@@ -12,7 +12,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities: AddEntitiesC
         TelraamSensor(coordinator, "car"),
         TelraamSensor(coordinator, "bike"),
         TelraamSensor(coordinator, "pedestrian"),
-        TelraamSensor(coordinator, "night")
+        TelraamSensor(coordinator, "night"),
+        TelraamSensor(coordinator, "v85"),
+        TelraamSensor(coordinator, "heavy"),
+        MotorizedTrafficSensor(coordinator)
     ], True)
 
 class TelraamSensor(CoordinatorEntity, SensorEntity):
@@ -22,8 +25,10 @@ class TelraamSensor(CoordinatorEntity, SensorEntity):
         self._traffic_type = traffic_type
         self._attributes = {}
         self._attr_unique_id = f"{coordinator.device_id}_{traffic_type}"  # Unique ID
-        if traffic_type == 'night':
-           self._attr_name = f"Night mode on"
+        if traffic_type == 'v85':
+            self._attr_name = f"85th Percentile Speed"
+        elif traffic_type == 'night':
+           self._attr_name = f"Night detections"
         else:
            self._attr_name = f"Telraam {traffic_type.capitalize()} Count"
 
@@ -34,6 +39,37 @@ class TelraamSensor(CoordinatorEntity, SensorEntity):
         if svalue is not None:
            return float(svalue)
         return svalue
+
+    @property
+    def state_class(self):
+        return "measurement"
+
+    @property
+    def device_info(self):
+        """Return information about the device."""
+        return DeviceInfo(
+            identifiers={(DOMAIN, self.coordinator.device_id)},
+            name=f"Telraam Traffic Counter ({self.coordinator.device_id})",
+            manufacturer="Telraam",
+            model="API v1",
+            configuration_url="https://telraam.net"
+        )
+
+    @property
+    def extra_state_attributes(self):
+        return self._attributes
+
+
+class MotorizedTrafficSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self.coordinator = coordinator
+        self._attributes = {}
+        self._attr_unique_id = f"{coordinator.device_id}_motorized"  # Unique ID
+        self._attr_name = f"Total motorized"
+    @property
+    def state(self):
+        return float(self.coordinator.data.get("car", 0)) + float(self.coordinator.data.get("night", 0)) + float(self.coordinator.data.get("heavy", 0))
 
     @property
     def state_class(self):
